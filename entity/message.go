36da2ccd29fff021eba3a29a97d43340c111e768
roskee <kirubeladamu@gmail.com>
2022-11-15 21:46:11 +0300

    added some more fields on message model

--- entity/message.go
+++ entity/message.go
@@ -6,12 +6,33 @@ import "strings"
 //
 // TODO: incomplete
 type Message struct {
-	MessageID      int64    `json:"message_id,omitempty"`
-	From           User     `json:"from,omitempty"`
-	Date           int64    `json:"date,omitempty"`
-	ForwardFrom    User     `json:"forward_from,omitempty"`
-	ReplyToMessage *Message `json:"reply_to_message,omitempty"`
-	Text           string   `json:"text,omitempty"`
+	// MessageID is a unique message identifier inside this chat.
+	//
+	// it is a required field.
+	MessageID int64 `json:"message_id,omitempty"`
+	// MessageThreadID is a unique identifier of a message thread to which the message belongs.
+	// it is for supergroups only
+	MessageThreadID int64 `json:"message_thread_id,omitempty"`
+	// From is the sender of the message.
+	// It is empty for messages sent to channels.
+	From *User `json:"from,omitempty"`
+	// SenderChat is the sender of the message, sent on behalf of a chat.
+	SenderChat *Chat `json:"sender_chat,omitempty"`
+	// Date is the date the message was sent in Unix time.
+	//
+	// It is a required field
+	Date int64 `json:"date,omitempty"`
+	// Chat is the conversation this message belongs to.
+	//
+	// It is a required field
+	Chat *Chat `json:"chat,omitempty"`
+	// ForwardFrom is, for forwarded messages, sender of the original message.
+	ForwardFrom *User `json:"forward_from,omitempty"`
+	// ForwardFromChat is,  For messages forwarded from channels or from anonymous administrators,
+	// information about the original sender chat
+	ForwardFromChat *Chat    `json:"forward_from_chat,omitempty"`
+	ReplyToMessage  *Message `json:"reply_to_message,omitempty"`
+	Text            string   `json:"text,omitempty"`
 }
 
 // GetCommand checks if this message has Text starting with '/'.
