061e5b40a5394a8838c23c8745d655c4c7f919c4
natnael-meresa <natnaelmeresa6@gmail.com>
2022-12-04 19:39:48 +0300

    add fields for sending chat action

--- entity/message_envelop.go
+++ entity/message_envelop.go
@@ -128,4 +128,12 @@ type MessageEnvelop struct {
 
 	// All Parameters for sendDice Method
 	*Dice
+
+	// Action is type of action to broadcast. Choose one, depending on what the user is about to receive.
+	// typing for text messages, upload_photo for photos, record_video or upload_video for videos,
+	// record_voice or upload_voice for voice notes, upload_document for general files, choose_sticker for stickers,
+	// find_location for location data, record_video_note or upload_video_note for video notes.
+	//
+	// It is a required field for sending a chat action.
+	Action string `json:"action"`
 }
