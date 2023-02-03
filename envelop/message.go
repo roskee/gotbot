ba2e17df1162ac03cb56f7d9df3cf61eb37da5d6
roskee <kirubeladamu@gmail.com>
2023-02-03 20:46:06 +0300

    added forward message envelop

---
+++ envelop/message.go
@@ -0,0 +1,26 @@
+package envelop
+
+// ForwardMessageEnvelop is used to forward a message
+type ForwardMessageEnvelop struct {
+	// ChatID is the id for the target chat or username.
+	//
+	// It is a required field.
+	ChatID string `json:"chat_id,omitempty"`
+	// MessageThreadID is the unique identifier for the target message thread
+	// (topic) of the forum;
+	// for forum super groups only.
+	MessageThreadID float64 `json:"message_thread_id,omitempty"`
+	// FromChatID id of the chat where the original message was sent.
+	//
+	// It is a required field.
+	FromChatID string `json:"from_chat_id,omitempty"`
+	// DisableNotification is to send the message silently.
+	DisableNotification bool `json:"disable_notification,omitempty"`
+	// ProtectContent is to protects the contents of the forwarded message
+	// from forwarding and saving.
+	ProtectContent bool `json:"protect_content,omitempty"`
+	// MessageID is the id of the message to forward.
+	//
+	// It is a required field.
+	MessageID float64 `json:"message_id,omitempty"`
+}
